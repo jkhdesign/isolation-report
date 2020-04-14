c879be9786349545f7d0e88e98c2f61dc8dd2dcd
Josh Krauth-Harding <jkrauth@collegeforcreativestudies.edu>
2020-04-14 10:43:34 -0400

    Homepage

--- assets/motion.js
+++ assets/motion.js
@@ -6,6 +6,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.fontSize = "0.5em";
     document.getElementById("logo").style.width = "20rem";
+    // document.getElementById("header").classList.remove("i");
   } else {
     document.getElementById("header").style.fontSize = "1em";
     document.getElementById("logo").style.width = "30rem";
